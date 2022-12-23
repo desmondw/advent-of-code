8b3b0addbe1dbb944cafe9cd524f100294f57ed4
Desmond Weindorf <desmondw@users.noreply.github.com>
2022-12-23 13:36:08 -0800

    .

--- 2022/12/code.py
+++ 2022/12/code.py
@@ -1,5 +1,9 @@
 f = open("example.txt", "r")
 # f = open("input.txt", "r")
 
+class Node():
+    def __init__(self):
+        self.path = []
+
 while (line := f.readline().rstrip().split(' ')) and len(line[0]):
     pass
