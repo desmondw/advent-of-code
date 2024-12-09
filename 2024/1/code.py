0c5cc6c1b2d4a60ae8ed7cdf346dfda7a10382a4
Desmond Weindorf <desmondw@users.noreply.github.com>
2024-12-08 18:38:15 -0800

    day 1

--- 2024/1/code.py
+++ 2024/1/code.py
@@ -1,5 +1,27 @@
 f = open("example.txt", "r")
-# f = open("input.txt", "r")
+f = open("input.txt", "r")
 
-while (line := f.readline().rstrip().split(' ')) and len(line[0]):
-    pass
+left = list()
+right = list()
+freq = dict()
+
+while (line := f.readline().rstrip().split('   ')) and len(line[0]):
+    left.append(l:=int(line[0]))
+    right.append(r:=int(line[1]))
+
+    if r not in freq:
+        freq[r] = 0
+    freq[r] += 1
+
+left.sort()
+right.sort()
+
+total = 0
+total2 = 0
+for i in range(len(left)):
+    total += abs(right[i] - left[i])
+    if left[i] in freq:
+        total2 += left[i] * freq[left[i]]
+
+print(total)
+print(total2)
